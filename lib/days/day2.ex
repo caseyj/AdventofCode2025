d14694f6ae811f1124ab202258a8dde314a46f6c
John Casey <john@jcasey.tech>
2025-12-02 15:29:41 -0500

    day 2 part 1 working, a tad slow.

--- lib/days/day2.ex
+++ lib/days/day2.ex
@@ -1,15 +1,42 @@
 defmodule Days.Day2 do
 
-
-  def generate_numbers_in_range(range_start, range_end) do
-    Enum.reduce(range_start..range_end, [], fn current,acc ->
-        acc ++ [Integer.to_string(current)]
-      end
-    )
+  def id_violation(id) do
+    str_length = String.length(id)
+    if Integer.mod(str_length, 2) == 0 do
+      half = div(str_length, 2)
+      String.slice(id, 0..half-1) == String.slice(id, half..str_length)
+    else
+      false
+    end
   end
 
-  def part1(_string_data) do
-    :ok
+  def range_str_to_count(str) do
+    string_to_range_params(str)
+    |> generate_numbers_in_range()
+    |> count_violations_in_range()
+  end
+
+  def sum_of_ranges(range_list) do
+    Enum.reduce(String.split(range_list, ","), 0, fn current, acc -> acc + range_str_to_count(current) end)
+  end
+
+  def count_violations_in_range(range) do
+    Enum.reduce(range, 0, fn current, acc -> if id_violation(current) do acc+(Integer.parse(current)|>elem(0)) else acc end end)
+  end
+
+
+  def string_to_range_params(str) do
+    String.split(str, "-")
+    |> Enum.map(fn current -> String.to_integer(current) end)
+    |> List.to_tuple()
+  end
+
+  def generate_numbers_in_range({range_start, range_end}) do
+    Enum.map(range_start..range_end, fn current -> Integer.to_string(current) end)
+  end
+
+  def part1(string_data) do
+    sum_of_ranges(String.trim(string_data))
   end
 
   def part2(_string_data) do
--- test/days/day2_test.exs
+++ test/days/day2_test.exs
@@ -10,9 +10,65 @@ defmodule Days.Day2Test do
 
   ] do
     test "Module function generate_numbers_in_range input #{inspect(input)} expected output #{inspect(expected_output)}" do
-      {range_start, range_end} = unquote(input)
-      assert Module.generate_numbers_in_range(range_start, range_end) == unquote(Macro.escape(expected_output))
+
+      assert Module.generate_numbers_in_range(unquote(input)) == unquote(Macro.escape(expected_output))
     end
+  end
+
+  for {input, expected_output} <- [
+    {"1-2", {1,2}},
+    {"1-3", {1,3}},
+    {"1-9", {1,9}},
+    {"11-19", {11,19}},
+  ] do
+    test "Module function string_to_range_params input #{inspect(input)} expected output #{inspect(expected_output)}" do
+      assert Module.string_to_range_params(unquote(input)) == unquote(Macro.escape(expected_output))
+    end
+  end
+
+  for {input, expected_output} <- [
+    {"12", false},
+    {"13", false},
+    {"19", false},
+    {"11", true},
+    {"22", true},
+    {"99", true},
+    {"1010", true},
+    {"1188511885", true},
+    {"446446", true},
+    {"38593859", true},
+  ] do
+    test "Module function id_violation input #{inspect(input)} expected output #{inspect(expected_output)}" do
+      assert Module.id_violation(unquote(input)) == unquote(Macro.escape(expected_output))
+    end
+  end
+
+  for {input, expected_output} <- [
+    {"11-22", 33},
+    {"95-115", 99},
+    {"998-1012", 1010},
+    {"1188511880-1188511890", 1188511885},
+    {"222220-222224", 222222},
+    {"1698522-1698528", 0},
+    {"446443-446449", 446446},
+    {"38593856-38593862", 38593859},
+  ] do
+    test "Module function range_str_to_count input #{inspect(input)} expected output #{inspect(expected_output)}" do
+      assert Module.range_str_to_count(unquote(input)) == unquote(Macro.escape(expected_output))
+    end
+  end
+
+  for {input, expected_output} <- [
+    {"11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124",1227775554 },
+  ] do
+    test "Module function sum_of_ranges input #{inspect(input)} expected output #{inspect(expected_output)}" do
+      assert Module.sum_of_ranges(unquote(input)) == unquote(Macro.escape(expected_output))
+    end
+  end
+
+  test "Day2 Part 1 test" do
+    {:ok, data} = File.read("test/data/Day2.txt")
+    assert Module.part1(data) == 1227775554
   end
 
 end
